22c46c468b3be2a450d1c447a452a6f237ceb0ea
vuongpn <trantungvuong@gmail.com>
2019-06-12 16:22:24 +0700

    add background for second screen

--- app/src/main/java/com/example/darkyskydemo/model/Datum__.kt
+++ app/src/main/java/com/example/darkyskydemo/model/Datum__.kt
@@ -3,4 +3,18 @@ package com.example.darkyskydemo.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class Datum__
+class Datum__{
+
+    @SerializedName("time")
+    @Expose
+    var time: Long? = null
+    @SerializedName("summary")
+    @Expose
+    var summary: String? = null
+    @SerializedName("temperatureMin")
+    @Expose
+    var temperatureMin: Double? = null
+    @SerializedName("temperatureMax")
+    @Expose
+    var temperatureMax: Double? = null
+}
--- app/src/main/java/com/example/darkyskydemo/secondscreen/DailyContract.kt
+++ app/src/main/java/com/example/darkyskydemo/secondscreen/DailyContract.kt
@@ -1,4 +1,5 @@
 package com.example.darkyskydemo.secondscreen
 
 class DailyContract {
+
 }
