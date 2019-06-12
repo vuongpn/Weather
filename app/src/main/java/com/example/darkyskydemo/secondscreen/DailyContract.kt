a3148abe848b8dc9b21fff22195c90de759d25cf
vuongpn <trantungvuong@gmail.com>
2019-06-12 16:32:50 +0700

    add contract , model

--- app/src/main/java/com/example/darkyskydemo/secondscreen/DailyContract.kt
+++ app/src/main/java/com/example/darkyskydemo/secondscreen/DailyContract.kt
@@ -1,5 +1,24 @@
 package com.example.darkyskydemo.secondscreen
 
-class DailyContract {
+import com.example.darkyskydemo.model.Datum__
+
+interface DailyContract {
+    interface View{
+        fun onSuccess(dailyList: List<Datum__>)
+        fun onFailure(t:Throwable)
+    }
+
+    interface Model{
+        interface OnFinishedListener{
+            fun onSuccess(dailyList: List<Datum__>)
+            fun onFailure(t:Throwable)
+        }
+        fun getData(OnFinishedListener:OnFinishedListener)
+    }
+
+    interface Presenter{
+        fun onDestroy()
+        fun requestData()
+    }
 
 }
--- app/src/main/java/com/example/darkyskydemo/secondscreen/DailyModel.kt
+++ app/src/main/java/com/example/darkyskydemo/secondscreen/DailyModel.kt
@@ -1,4 +1,29 @@
 package com.example.darkyskydemo.secondscreen
 
-class DailyModel {
+import com.example.darkyskydemo.model.Weather
+import com.example.darkyskydemo.network.ApiClient
+import com.example.darkyskydemo.network.ApiClient.API_KEY
+import com.example.darkyskydemo.network.ApiClient.latitude
+import com.example.darkyskydemo.network.ApiClient.longtitude
+import com.example.darkyskydemo.network.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DailyModel:DailyContract.Model {
+    override fun getData(OnFinishedListener: DailyContract.Model.OnFinishedListener) {
+       val apiService=ApiClient.client!!.create(ApiInterface::class.java)
+
+        apiService.getForecastData(API_KEY, latitude, longtitude).enqueue(object : Callback<Weather>{
+            override fun onFailure(call: Call<Weather>, t: Throwable) {
+               OnFinishedListener.onFailure(t)
+            }
+
+            override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
+               val dailyList= response.body()!!.daily!!.data
+                OnFinishedListener.onSuccess(dailyList!!)
+            }
+
+        })
+    }
 }
